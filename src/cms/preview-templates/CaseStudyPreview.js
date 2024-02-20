a16d25afbbcf4e7eb5a2df687e2b8a0f8f99a50f
prtkjakhar <prtkjakhar@gmail.com>
2024-02-20 20:46:55 +0530

    fixes pushed

--- src/cms/preview-templates/CaseStudyPreview.js
+++ src/cms/preview-templates/CaseStudyPreview.js
@@ -3,19 +3,11 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 const CaseStudyPreview = ({ entry, widgetFor }) => {
-  console.log("hello", entry)
   const data = entry.getIn(['data']).toJS();
-  const { markdownRemark: post } = data;
   if (data) {
     return (
       <CaseStudyTemplate
-        content={post.frontmatter}
-        helmet={
-          <Helmet titleTemplate="%s | CaseStudy">
-            <title>{`preview`}</title>
-            <meta name="description" content={`preview`} />
-          </Helmet>
-        }
+        content={data}
       />
     );
   } else {
